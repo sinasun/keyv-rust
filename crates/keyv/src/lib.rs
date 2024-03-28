391b944ee84f4d6473306ee25b26c5ec7a42de10
sinasun <sinakhodaveisi@gmail.com>
2024-03-28 03:26:14 -0400

    Interface added

--- crates/keyv/src/lib.rs
+++ crates/keyv/src/lib.rs
@@ -1,8 +1,15 @@
 mod store;
+mod helpers;
 
 use crate::store::Store;
 
+use serde::{Deserialize, Serialize};
+
 struct Keyv<T: Store> {
+    store: T,
+}
+
+struct KeyvAsync<T: Store> {
     store: T,
 }
 
@@ -11,14 +18,60 @@ impl<T: Store> Keyv<T> {
         Keyv { store }
     }
 
-    pub async fn get<V>(&self, key: &str) -> Result<V, String>
-    where
-        V: for<'de> serde::Deserialize<'de> + Send + Sync,
-    {
-        self.store.get(key).await
+    pub fn get_many<'a, K, V>(&self, keys: Vec<&str>) -> Result<Vec<V>, String>
+    where 
+        V: for<'de> Deserialize<'de> {
+        let mut result: Vec<V> = vec![];
+    for key in keys {
+            result.push(self.get(key).unwrap());
+        };
+        Ok(result)
     }
 
-    pub async fn set(&self, key: &str, value: &str) -> Result<bool, String> {
-        self.store.set(key, value).await
+    pub fn get<'a, V>(&self, key: &str) -> Result<V, String>
+    where
+        V: for<'de> Deserialize<'de>
+    {
+        let deserialized: V = serde_json::from_str(self.store.get(key).unwrap()).unwrap();
+        Ok(deserialized)
     }
+
+
+    pub fn set<V>(&self, key: &str, value: V)
+    where V: Serialize
+    {
+        self.store.set(key, &serde_json::to_string(&value).unwrap())
+    }
+
+}
+
+impl<T: Store> KeyvAsync<T> {
+    pub fn new(store: T) -> Self {
+        KeyvAsync {store}
+    }
+
+    pub async fn get<V>(&self, key: &str) -> Result<V, String>
+    where
+        V: for<'de> Deserialize<'de> + Send + Sync,
+    {
+        let deserialized: V = serde_json::from_str(self.store.get_async(key).await.unwrap()).unwrap();
+        Ok(deserialized)
+    }
+
+    pub async fn get_many<'a, K, V>(&self, keys: Vec<&str>) -> Result<Vec<V>, String>
+    where 
+        V: for<'de> Deserialize<'de> + Send + Sync {
+        let mut result: Vec<V> = vec![];
+    for key in keys {
+            result.push(self.get(key).await.unwrap());
+        };
+        Ok(result)
+    }
+
+    pub async fn set<V>(&self, key:&str, value:V) -> Result<bool, String> 
+    where V: Serialize + Send
+    {
+        self.store.set_async(key, &serde_json::to_string(&value).unwrap()).await
+    }
+
 }
--- crates/keyv/src/store/mod.rs
+++ crates/keyv/src/store/mod.rs
@@ -1,11 +1,17 @@
 use async_trait::async_trait;
-use serde::Deserialize;
 
 #[async_trait]
 pub trait Store {
-    async fn get<V>(&self, key: &str) -> Result<V, String>
-    where
-        V: for<'de> Deserialize<'de> + Send + Sync;
 
-    async fn set(&self, key: &str, value: &str) -> Result<bool, String>;
+    fn get(&self, key: &str) -> Result<&str, String>;
+    fn set(&self, key: &str, value: &str);
+
+    fn clear();
+
+
+    // Async Functions
+    async fn get_async(&self, key: &str) -> Result<&str, String>;
+    async fn set_async(&self, key: &str, value: &str) -> Result<bool, String>;
+
+    async fn clear_async();
 }
